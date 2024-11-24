ec458840dad840285d7b671ea8298d12a857c767
Carlo <93345815+Finn331@users.noreply.github.com>
2024-11-24 14:55:57 +0700

    FPS Engine
    disini gw menambahkan Asset FPS Engine

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -4,19 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public FixedJoystick joystick;
-    public float moveSpeed = 5f;
-    
+    [SerializeField] FixedJoystick joystick;
+    public float moveSpeed;
+    public float jumpForce;
+
     private CharacterController controller;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    // Panjang ray untuk deteksi tanah
+    public float groundCheckDistance = 0.1f;
+
+    // LayerMask untuk mendeteksi ground
+    [SerializeField] LayerMask groundLayer;
+
+    // GameObject untuk titik awal raycast
+    [SerializeField] Transform groundCheck;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    
     void Update()
     {
-        Vector3 Move = transform.right * joystick.Horizontal + transform.forward * joystick.Vertical;
-        controller.Move(Move * moveSpeed * Time.deltaTime);
+        // Pergerakan pemain
+        Vector3 move = transform.right * joystick.Horizontal + transform.forward * joystick.Vertical;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Deteksi apakah pemain berada di tanah menggunakan groundCheck
+        if (groundCheck != null)
+        {
+            isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundCheckDistance, groundLayer);
+        }
+        else
+        {
+            Debug.LogWarning("GroundCheck transform belum diatur!");
+        }
+
+        // Debug ray untuk melihat deteksi tanah di Scene view
+        if (groundCheck != null)
+        {
+            Debug.DrawRay(groundCheck.position, Vector3.down * groundCheckDistance, isGrounded ? Color.green : Color.red);
+        }
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
